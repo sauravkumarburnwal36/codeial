d4e0dcad070dd6bba0ed7edacd8dde3a292407aa
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2022-07-05 18:58:21 +0530

    First Commit:Create the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,16 @@
+//To require express
+const express= require('express');
+
+//To acquire the functionalities of express libraries
+const app= express();
+//To define the port to run the server
+const port= 8000;
+
+//What need to be done when server is fired
+app.listen(port,function(err){
+    if(err){
+        //Concatenate the String using interpolation {} is used to embed the string and string is placed inside it
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is running on the port: ${port}`);
+});
