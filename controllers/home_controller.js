b5a34d20b7ea52152640065f7d02c7afb1b738b2
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2022-07-05 22:24:46 +0530

    created view engine for home and users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,8 @@
 //as it is an object
 module.exports.home= function(req,res){
-    return res.end('<h1>Express is up for codeial</h1>');
+    return res.render('home',{
+        title:"Home"
+    });
 }
 
 //module.exports.actionName= functione(req,res){}
