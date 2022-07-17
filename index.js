327237ae1ff985e747c02b54575f69774a907d5d
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2022-07-17 22:58:58 +0530

    extract the style and script from subpages into the layout file

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const expressLayouts=require('express-ejs-layouts');
 //to use static files
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+//extract styles and scripts from subpages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 //setup the view engine
 app.set('view engine','ejs');
