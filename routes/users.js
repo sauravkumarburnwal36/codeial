ffc587d380616c627d84269970177ce62085eb3e
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2022-07-17 20:34:25 +0530

    modified users files under routes

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ const express= require('express');
 const router= express.Router();
 
 //to access user_controller
-const userController= require('../controllers/user_controller');
+const usersController= require('../controllers/users_controller');
 
 //to access users/profile
-router.get('/profile',userController.profile);
+router.get('/profile',usersController.profile);
 
 module.exports=router;
