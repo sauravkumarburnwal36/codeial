48038483438cbe2c16d8fca0eaba7d089a31b015
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2022-07-19 22:50:47 +0530

    setup mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose= require('mongoose');
+
+//to make a connection
+mongoose.connect('mongodb://localhost/codeial_development');
+
+const db= mongoose.connection;
+
+db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
+db.once('open',function(){
+    console.log("Connected to Database:MongoDB");
+});
+module.exports=db;
