3adfb3c7981297efb00201c3801123bc65476ffd
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2022-07-22 18:08:56 +0530

    setup manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,28 @@
 //to require users schema
 const User= require('../models/user');
 module.exports.profile= function(req,res){
-   return res.render('user_profile',{
-    title:'User Profile'
+   //if user is exists under cookies
+   if(req.cookies.user_id){
+      User.findById(req.cookies.user_id,function(err,user){
+         if(user){
+            return res.render('user_profile',{
+               title:"User Profile",
+               user:user
+            })
+         }else{
+            return res.redirect('/users/sign-in');
+
+        }
+      });
+   }else{
+      return res.redirect('/users/sign-in');
+   }
+}
+
+//render sign up page
+module.exports.signUp= function(req,res){
+   return res.render('user_sign_up',{
+      title:'Codeial|Sign Up'
    })
 }
 
@@ -10,13 +30,6 @@ module.exports.profile= function(req,res){
 module.exports.signIn= function(req,res){
    return res.render('user_sign_in',{
       title:'Codeial|Sign In'
-   })
-}
-
-//render sign up page
-module.exports.signUp= function(req,res){
-   return res.render('user_sign_up',{
-      title:'Codeial|Sign Up'
    })
 }
 
@@ -48,5 +61,27 @@ module.exports.create= function(req,res){
 
 //sign in and create session for the user
 module.exports.createSession= function(req,res){
-   //TODO Later
+   //steps to authenticate
+   //find the user
+   User.findOne({email:req.body.email},function(err,user){
+      if(err){
+         console.log('error in finding  user in signing in');
+         return;
+      }
+      //handle user found
+      if(user){
+         //handle password which doesn't match
+         if(user.password!=req.body.password){
+            return res.redirect('back');
+         }
+         //handle session creation
+         res.cookie('user_id',user.id);
+         return res.redirect('/users/profile');
+      }
+      else
+      {
+         //handle user not found
+         return res.redirect('back');
+      }
+   });
 }
