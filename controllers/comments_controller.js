42780c3ad2dc5e57fa6f6f9811f90568837055a3
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2022-07-29 23:49:29 +0530

    added code for deleting post and comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -20,3 +20,22 @@ module.exports.create= function(req,res){
         }
     });
 };
+
+module.exports.destroy= function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+        if(comment.user == req.user.id){
+
+            //fetching the post if from comment
+            let postId= comment.post;
+
+            comment.remove();
+            //pull out the comment from list of comment
+            Post.findByIdAndUpdate(postId,{$pull: {comments:req.params.id}},function(err,post){
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
+}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post= require('../models/post');
+const Comment= require('../models/comment');
 
 module.exports.create= function(req,res){
     Post.create({
@@ -12,3 +13,18 @@ module.exports.create= function(req,res){
         return res.redirect('back');
     });
 };
+
+module.exports.destroy= function(req,res){
+    Post.findById(req.params.id,function(err,post){
+       //mongoose gives the facility--> .id means converting the object id i.e, _id to string
+       if(post.user == req.user.id){
+        post.remove();
+        Comment.deleteMany({post:req.params.id},function(err){
+            return res.redirect('back');
+        })
+       } 
+       else{
+        return res.redirect('back');
+       }
+    });
+}
