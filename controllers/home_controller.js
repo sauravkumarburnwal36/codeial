d945ba770c2135280abca53a764f4d96e313f2c2
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2022-07-31 23:34:31 +0530

    adding async-await to reduce callback hell  and implemented error handling techniques

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,33 +3,62 @@ const { populate } = require('../models/post');
 const Post= require('../models/post');
 const User= require('../models/user');
 
-module.exports.home= function(req,res){
-    // console.log(req.cookies);
-    // res.cookie('user_id',36);
+// module.exports.home= function(req,res){
+//     // console.log(req.cookies);
+//     // res.cookie('user_id',36);
 
-    // The query returns all the posts -->Populate the user of each posts.
-   Post.find({})
-   .populate('user')
-   .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-   })
-   .exec(function(err,posts){
+//     // The query returns all the posts -->Populate the user of each posts.
+//    Post.find({})
+//    .populate('user')
+//    .populate({
+//         path:'comments',
+//         populate:{
+//             path:'user'
+//         }
+//    })
+//    .exec(function(err,posts){
        
-        User.find({},function(err,users){
+//         User.find({},function(err,users){
 
-            return res.render('home',{
-                title:"Codeial|Home",
-                posts: posts,
-                all_users: users
-            });
+//             return res.render('home',{
+//                 title:"Codeial|Home",
+//                 posts: posts,
+//                 all_users: users
+//             });
 
-        });
+//         });
             
     
-   });
-};
+//    });
+// };
+
+//using async await to reduce callback hell scenarios
+module.exports.home= async function(req,res){
+    try{
+        //populate the users of each post
+        let posts= await Post.find({})
+                    .populate('user')
+                    .populate({
+                        path:'comments',
+                        populate:{
+                            path:'user'
+                        }
+                    });
+        let users= await User.find({});
+            
+        return res.render('home',{
+            title: "Codeial|Home",
+            posts: posts,
+            all_users: users
+        });
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
+}
+
+
+
+
 
 //module.exports.actionName= functione(req,res){}
